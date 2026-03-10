09bb04aaf659009323be9e550cdd1c28453c7971
William J G Pettifer <billy.pettifer@gmail.com>
2026-03-10 15:05:02 +0000

    misc

--- src/cursor.gd
+++ src/cursor.gd
@@ -3,5 +3,5 @@ extends Node2D
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
-		position.y = event.position.y
-		position.x = clamp(event.position.x, 0, 540)
+		position.y = clamp(event.position.y, 5, 535)
+		position.x = clamp(event.position.x, 5, 535)
